f31f12eb582086a9a435440f931b2b46435a4009
Munyoung Hwang <munyoung.hwang@navercorp.com>
2019-06-05 01:05:02 +0900

    add bootstrap.el

--- init.el
+++ init.el
@@ -1,18 +1,1 @@
-;; stuff automatically added
-;;
-
-;; Our stuff
-
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
-
-(package-initialize)
-		    
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
+(load "~/.emacs.d/bootstrap.el")
